55fb2833a99916f7fec3878b684429dcef7db1ff
Ian Field <ian.field90@gmail.com>
2015-08-25 14:23:55 +0100

    Initial Commit

---
+++ DevStat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DevStat
+//
+//  Created by Ian Field on 25/08/2015.
+//  Copyright (c) 2015 Ian Field. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DevStat/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  DevStat
+//
+//  Created by Ian Field on 25/08/2015.
+//  Copyright (c) 2015 Ian Field. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
